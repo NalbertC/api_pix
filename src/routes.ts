54c9594a8c1f52023311f78f1f42dd308bfaf39d
NalbertC <nalberthcastro1510@gmail.com>
2023-04-05 21:42:17 -0300

    routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,7 @@ import { cobranca, pix, qrCode, webhook } from "./services/gerencianet";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  return res.status(200).json({ hello: "world!" });
-});
-
-router.get("/qrcode", qrCode);
+router.get("/", qrCode);
 router.get("/cob", cobranca);
 router.post("/webhook(/pix)?", webhook);
 router.get("/pix", pix);
